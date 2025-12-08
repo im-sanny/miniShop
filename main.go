089d337cd1201a94852d757880d945e49ae6cb2d
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-12-08 22:44:35 +0600

    feat(method): implemented get method with mux

--- main.go
+++ main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-func shopHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Mini Shop!")
+// album represents data about a record album
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
-func ownerHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Theo Fumis")
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "give me get req", 400)
+	}
+	encoder := json.NewEncoder(w)
+	encoder.Encode(albums)
 }
 
 func main() {
-	http.HandleFunc("/", shopHandler)
-	http.HandleFunc("/owner", ownerHandler)
+	mux := http.NewServeMux()
 
-	err := http.ListenAndServe(":3001", nil)
+	mux.HandleFunc("/albums", getAlbums)
+
+	err := http.ListenAndServe(":3001", mux)
 	// this will catch error if theres any while running the server
 	if err != nil {
 		fmt.Println("Error starting the server", err)
